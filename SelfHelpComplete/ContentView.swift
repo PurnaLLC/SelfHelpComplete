a6dd036a9310c4a7608465d72bd1960f25a8bb4c
ThunderSmoke <116113740+ThunderSmoke@users.noreply.github.com>
2023-12-21 08:19:37 -0600

    Initial Commit

---
+++ SelfHelpComplete/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SelfHelpComplete
+//
+//  Created by Maxwell Meyer on 12/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SelfHelpComplete/SelfHelpCompleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfHelpCompleteApp.swift
+//  SelfHelpComplete
+//
+//  Created by Maxwell Meyer on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct SelfHelpCompleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
